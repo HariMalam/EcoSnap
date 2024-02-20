feb3ad4ceeb038b02c4713de858c2c0095a3f0b6
Hari <116895144+HariMalam@users.noreply.github.com>
2024-02-20 15:20:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ const User = require('./models/user');
 const Form = require('./models/form');
 
 const app = express();
-
-const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://hari:Hari5291@cluster0.efp2vcw.mongodb.net/ecosnap' || 'mongodb://127.0.0.1:27017/ecosnap';
+const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://hari:Hari5291@cluster0.efp2vcw.mongodb.net/ecosnap';
+// const mongoURI = 'mongodb://127.0.0.1:27017/ecosnap';
 mongoose.connect(mongoURI);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
